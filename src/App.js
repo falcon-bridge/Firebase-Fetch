be0ddf2a00878daf00936acac415cf90b9fb7168
Aayush <falcon.bridge2000@gmail.com>
2022-01-23 20:19:11 +0530

    removed a samll bug to get the release date too

--- src/App.js
+++ src/App.js
@@ -33,16 +33,6 @@ function App() {
         });
       }
 
-      console.log(data);
-
-      // const transformedMovies = data.results.map((movieData) => {
-      //   return {
-      //     id: movieData.episode_id,
-      //     title: movieData.title,
-      //     openingText: movieData.opening_crawl,
-      //     releaseDate: movieData.release_date,
-      //   };
-      // });
       setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
